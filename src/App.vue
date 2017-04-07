163344c43adefcfacc5cdee23bae72dc4dfd9baf
Danilo Almeida <danilolpa@gmail.com>
2017-04-07 15:48:43 -0300

    Adicionado Projeto

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- Don't drop "q-app" class -->
+  <div id="q-app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+/*
+ * Root component
+ */
+export default {}
+</script>
+
+<style lang="styl">
+  body{
+    background: #fcfcfc
+  }
+</style>
---
+++ src/components/Index.vue
@@ -0,0 +1,39 @@
+<template>
+  <q-layout>
+    <div slot="header" class="toolbar tertiary">
+      <q-toolbar-title :padding="10">
+        <i class="on-left">
+          done_all
+        </i>
+        Tarefas do Dia
+      </q-toolbar-title>
+    </div>
+
+    <div slot="header" class="toolbar tertiary">
+        <q-toolbar-title>
+          Hoje
+        </q-toolbar-title>
+    </div>
+      <div class="layout-view"><cards-list></cards-list></div>
+
+  </q-layout>
+</template>
+
+<script>
+import CardsList from './CardsList'
+
+export default {
+  data () {
+    return {
+      search: ''
+    }
+  },
+  components: {
+    CardsList
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
